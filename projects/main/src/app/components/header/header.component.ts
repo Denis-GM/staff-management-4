36d8bf1a33bd93c4c47e74f1df4505c909c60dfa
Denis <d.pleshivtsev1@gmail.com>
2023-04-23 19:21:08 +0500

    logout

--- projects/main/src/app/components/header/header.component.ts
+++ projects/main/src/app/components/header/header.component.ts
@@ -1,9 +1,24 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { LocalStorageService } from '../../services/local-storage.service';
+import { IUser } from '../../interfaces/user.interface';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent{
+export class HeaderComponent {
+  user: IUser = this.localStorageService.get('user');
+
+  constructor(
+    private localStorageService: LocalStorageService,
+    private router: Router
+  ) {}
+
+  logout(): void {
+    this.localStorageService.remove('user');
+    this.router.navigate(['/login'])
+  }
 
 }
